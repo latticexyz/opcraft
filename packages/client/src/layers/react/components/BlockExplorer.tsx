c2f17b8a7949a5aaa1deac5b5a6959dfa198c412
Kevin Ingersoll <kingersoll@gmail.com>
2022-09-24 16:50:41 -0700

    wip

--- packages/client/src/layers/react/components/BlockExplorer.tsx
+++ packages/client/src/layers/react/components/BlockExplorer.tsx
@@ -1,6 +1,7 @@
 import { NetworkComponentUpdate } from "@latticexyz/network";
 import React from "react";
 import { scan } from "rxjs";
+import { BlockIdToKey } from "../../network/constants";
 import { registerUIComponent } from "../engine";
 
 export function registerBlockExplorer() {
@@ -14,22 +15,36 @@ export function registerBlockExplorer() {
     },
     (layers) => {
       const {
-        network: { ecsEvent$ },
+        network: {
+          ecsEvent$,
+          mappings,
+          components: { Item },
+        },
+        noa: { world },
       } = layers;
 
       // Group stream of component update events by block number
       return ecsEvent$.pipe(
-        scan<NetworkComponentUpdate, NetworkComponentUpdate[][]>((acc, curr) => {
-          const [prev, ...rest] = acc;
-          const prevBlockNumber = (prev && prev[0]?.blockNumber) ?? 0;
-          if (curr.blockNumber > prevBlockNumber) {
-            return [[curr], ...acc].slice(0, 1);
-          }
-          return [[curr, ...(prev || [])], ...rest].slice(0, 1);
-        }, [])
+        scan<NetworkComponentUpdate, Record<string, Record<string, NetworkComponentUpdate[]>>>((acc, curr) => {
+          const { blockNumber, txHash } = curr;
+          // const entityIndex = world.entityToIndex.get(curr.entity);
+          // const componentKey = mappings[curr.component];
+          // const update = curr as NetworkComponentUpdate<Item>;
+          // // console.log({ componentKey }, curr);
+          // if (componentKey === "Item") {
+          //   const blockType = BlockIdToKey[curr.value.value];
+          //   // console.log({ entityIndex, componentKey, blockType }, curr);
+          // }
+          acc[blockNumber] = acc[blockNumber] || {};
+          acc[blockNumber][txHash] = acc[blockNumber][txHash] || [];
+          acc[blockNumber][txHash].push(curr);
+          console.log(acc);
+          return acc;
+        }, {})
       );
     },
     (state) => {
+      console.log("BlockExplorer state", state);
       return (
         <div>
           <p>Event</p>
