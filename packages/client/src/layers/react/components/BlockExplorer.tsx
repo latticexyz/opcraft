85941b73aec1b4d333af0a7eba18a12e9173eb3b
Kevin Ingersoll <kingersoll@gmail.com>
2022-09-27 16:02:51 -0700

    more fiddling

--- packages/client/src/layers/react/components/BlockExplorer.tsx
+++ packages/client/src/layers/react/components/BlockExplorer.tsx
@@ -1,8 +1,15 @@
 import { isNetworkComponentUpdateEvent, NetworkComponentUpdate } from "@latticexyz/network";
+import { ComponentValue, isComponentUpdate } from "@latticexyz/recs";
 import React from "react";
-import { filter, scan } from "rxjs";
+import { filter, scan, merge } from "rxjs";
 import { BlockIdToKey } from "../../network/constants";
 import { registerUIComponent } from "../engine";
+
+type BlockSummary = {
+  [blockNumber: string]: {
+    [blockType: string]: number;
+  };
+};
 
 export function registerBlockExplorer() {
   registerUIComponent(
@@ -19,14 +26,35 @@ export function registerBlockExplorer() {
           ecsEvent$,
           mappings,
           components: { Item },
+          network: { blockNumber$ },
         },
         noa: { world },
       } = layers;
 
+      console.log("Item.id", Item.id);
+
       // Group stream of component update events by block number
-      return ecsEvent$.pipe(filter(isNetworkComponentUpdateEvent)).pipe(
-        scan<NetworkComponentUpdate, Record<string, Record<string, NetworkComponentUpdate[]>>>((acc, curr) => {
-          const { blockNumber, txHash } = curr;
+      return merge(blockNumber$, ecsEvent$.pipe(filter(isNetworkComponentUpdateEvent))).pipe(
+        scan<NetworkComponentUpdate | number, BlockSummary>((summary, update) => {
+          // Block number
+          if (typeof update === "number") {
+            summary[update] = summary[update] || {};
+            return summary;
+          }
+
+          // otherwise it's a NetworkComponentUpdate
+          const { blockNumber, txHash } = update;
+          // We're only visualizing the stream of new blocks so skip backfill
+          if (txHash === "worker" || txHash === "cache") return summary;
+
+          const componentKey = mappings[update.component];
+          if (componentKey === Item.id) {
+            const value = update.value as ComponentValue<Item>;
+            const blockType = BlockIdToKey[update.value.value];
+          }
+
+          console.log("network update", update);
+
           // const entityIndex = world.entityToIndex.get(curr.entity);
           // const componentKey = mappings[curr.component];
           // const update = curr as NetworkComponentUpdate<Item>;
@@ -35,16 +63,18 @@ export function registerBlockExplorer() {
           //   const blockType = BlockIdToKey[curr.value.value];
           //   // console.log({ entityIndex, componentKey, blockType }, curr);
           // }
-          acc[blockNumber] = acc[blockNumber] || {};
-          acc[blockNumber][txHash] = acc[blockNumber][txHash] || [];
-          acc[blockNumber][txHash].push(curr);
-          console.log(acc);
-          return acc;
+
+          // acc[blockNumber] = acc[blockNumber] || {};
+          // acc[blockNumber].transactions[txHash] = acc[blockNumber].transactions[txHash] || [];
+          // acc[blockNumber].transactions[txHash].push(curr);
+
+          // console.log("updates for tx", txHash, acc[blockNumber].transactions[txHash]);
+          return summary;
         }, {})
       );
     },
     (state) => {
-      console.log("BlockExplorer state", state);
+      // console.log("BlockExplorer state", state);
       return (
         <div>
           <p>Event</p>
