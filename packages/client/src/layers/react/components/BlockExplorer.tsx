b385e33cb30b54b85c0ec8c47209e5059dd74e2a
alvrs <alvarius@lattice.xyz>
2022-09-28 01:02:59 +0100

    feat: block explorer data aggregation

--- packages/client/src/layers/react/components/BlockExplorer.tsx
+++ packages/client/src/layers/react/components/BlockExplorer.tsx
@@ -1,15 +1,21 @@
-import { isNetworkComponentUpdateEvent, NetworkComponentUpdate } from "@latticexyz/network";
-import { ComponentValue, isComponentUpdate } from "@latticexyz/recs";
 import React from "react";
-import { filter, scan, merge } from "rxjs";
-import { BlockIdToKey } from "../../network/constants";
+import { isNetworkComponentUpdateEvent, NetworkComponentUpdate } from "@latticexyz/network";
+import { ComponentValue, EntityID, getComponentValue, SchemaOf } from "@latticexyz/recs";
+import { filter, scan, merge, map } from "rxjs";
+import { BlockIdToKey, BlockTypeKey } from "../../network/constants";
 import { registerUIComponent } from "../engine";
 
-type BlockSummary = {
-  [blockNumber: string]: {
-    [blockType: string]: number;
-  };
-};
+type BlockSummary = { blockNumber: number; mine: BlockTypeKey[]; build: BlockTypeKey[] }[];
+
+const NUM_BLOCKS = 5;
+
+function count(items: string[]) {
+  return items.reduce<{ [key: string]: number }>((acc, curr) => {
+    acc[curr] = acc[curr] || 0;
+    acc[curr]++;
+    return acc;
+  }, {});
+}
 
 export function registerBlockExplorer() {
   registerUIComponent(
@@ -27,54 +33,70 @@ export function registerBlockExplorer() {
           mappings,
           components: { Item },
           network: { blockNumber$ },
+          world,
         },
-        noa: { world },
       } = layers;
-
-      console.log("Item.id", Item.id);
 
       // Group stream of component update events by block number
       return merge(blockNumber$, ecsEvent$.pipe(filter(isNetworkComponentUpdateEvent))).pipe(
         scan<NetworkComponentUpdate | number, BlockSummary>((summary, update) => {
           // Block number
           if (typeof update === "number") {
-            summary[update] = summary[update] || {};
-            return summary;
+            const block = summary.find((e) => e.blockNumber === update);
+            if (!block) summary.push({ blockNumber: update, mine: [], build: [] });
+            return summary.slice(-NUM_BLOCKS);
           }
 
           // otherwise it's a NetworkComponentUpdate
           const { blockNumber, txHash } = update;
+
           // We're only visualizing the stream of new blocks so skip backfill
           if (txHash === "worker" || txHash === "cache") return summary;
 
-          const componentKey = mappings[update.component];
-          if (componentKey === Item.id) {
-            const value = update.value as ComponentValue<Item>;
-            const blockType = BlockIdToKey[update.value.value];
+          // Find the block entry for this block
+          let block = summary.find((e) => e.blockNumber === blockNumber);
+          if (!block) {
+            block = { blockNumber, mine: [], build: [] };
+            summary.push(block);
           }
 
-          console.log("network update", update);
+          const componentKey = mappings[update.component];
 
-          // const entityIndex = world.entityToIndex.get(curr.entity);
-          // const componentKey = mappings[curr.component];
-          // const update = curr as NetworkComponentUpdate<Item>;
-          // // console.log({ componentKey }, curr);
-          // if (componentKey === "Item") {
-          //   const blockType = BlockIdToKey[curr.value.value];
-          //   // console.log({ entityIndex, componentKey, blockType }, curr);
-          // }
+          // Item component updates correspond to a mined terrain block
+          if (componentKey === "Item") {
+            const { value } = update.value as ComponentValue<SchemaOf<typeof Item>>;
+            block.mine.push(BlockIdToKey[value as EntityID]);
+            return summary.slice(-NUM_BLOCKS);
+          }
 
-          // acc[blockNumber] = acc[blockNumber] || {};
-          // acc[blockNumber].transactions[txHash] = acc[blockNumber].transactions[txHash] || [];
-          // acc[blockNumber].transactions[txHash].push(curr);
+          // Position component updates correspond to a mined or placed ECS block
+          if (componentKey === "Position") {
+            const entityIndex = world.entityToIndex.get(update.entity);
+            const blockType = entityIndex != null && getComponentValue(Item, entityIndex)?.value;
+            if (!blockType) {
+              console.warn("Received Position update of unknown entity", update.entity);
+              return summary;
+            }
 
-          // console.log("updates for tx", txHash, acc[blockNumber].transactions[txHash]);
-          return summary;
-        }, {})
+            // If the update includes a position, it corresponds to a placed block
+            if (update.value) block.build.push(BlockIdToKey[blockType as EntityID]);
+            // otherwise it corresponds to a mined ecs block
+            else block.mine.push(BlockIdToKey[blockType as EntityID]);
+          }
+
+          return summary.slice(-NUM_BLOCKS);
+        }, []),
+        // Count blocks
+        map((summary) => {
+          return summary.map(({ blockNumber, mine, build }) => ({
+            blockNumber,
+            mine: count(mine.filter((e) => e !== "Air")),
+            build: count(build.filter((e) => e != "Air")),
+          }));
+        })
       );
     },
     (state) => {
-      // console.log("BlockExplorer state", state);
       return (
         <div>
           <p>Event</p>
