d7af27a4b64626c438aaa30680bde7eb3899c71e
alvrs <alvarius@lattice.xyz>
2022-09-30 20:44:25 +0100

    feat: add mvp UI for address/balance

---
+++ packages/client/src/layers/react/components/Balance.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { registerUIComponent } from "../engine";
+import { concat, interval, map, of } from "rxjs";
+import styled from "styled-components";
+import { utils } from "ethers";
+import { awaitPromise } from "@latticexyz/utils";
+
+export function registerBalance() {
+  registerUIComponent(
+    "Crosshairs",
+    {
+      rowStart: 1,
+      rowEnd: 2,
+      colStart: 8,
+      colEnd: 13,
+    },
+    ({
+      network: {
+        network: { signer, connectedAddress },
+      },
+    }) => {
+      return concat(of(0), interval(5000)).pipe(
+        map(async () => {
+          const balance = (await signer.get()?.getBalance()) ?? 0;
+          return utils.formatEther(balance);
+        }),
+        awaitPromise(),
+        map((balance) => ({ balance, address: connectedAddress.get() }))
+      );
+    },
+    ({ balance, address }) => {
+      return (
+        <Wrapper>
+          address: {address} / balance: {balance}
+        </Wrapper>
+      );
+    }
+  );
+}
+
+const Wrapper = styled.div``;
