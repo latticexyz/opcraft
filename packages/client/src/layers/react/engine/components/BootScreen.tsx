3bd01cc7fc89d6f42e4c718a4bc6ebc1bfd46a76
alvrs <alvarius@lattice.xyz>
2022-10-20 18:59:41 +0100

    feat: reduce loading screen opacity to show loading world

--- packages/client/src/layers/react/engine/components/BootScreen.tsx
+++ packages/client/src/layers/react/engine/components/BootScreen.tsx
@@ -20,7 +20,7 @@ const Container = styled.div`
   width: 100%;
   height: 100%;
   position: absolute;
-  background-color: #000;
+  background-color: rgb(0 0 0 / 90%);
   display: grid;
   align-content: center;
   align-items: center;
