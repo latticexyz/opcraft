f7e7b554983905d89f79e5d8f51f9f179fd93c51
Kevin Ingersoll <kingersoll@gmail.com>
2022-11-02 09:52:15 -0700

    feat: make water translucent

--- packages/client/src/layers/phaser/assets/tilesets/opcraftTileset.ts
+++ packages/client/src/layers/phaser/assets/tilesets/opcraftTileset.ts
@@ -4,7 +4,6 @@ export const BackgroundTiles: Partial<Record<keyof typeof BlockType, number>> = 
   Grass: 1,
   Dirt: 2,
   Sand: 3,
-  Water: 4,
   Stone: 5,
   Coal: 6,
   Snow: 7,
@@ -14,6 +13,7 @@ export const BackgroundTiles: Partial<Record<keyof typeof BlockType, number>> = 
 };
 
 export const ForegroundTiles: Partial<Record<keyof typeof BlockType, number>> = {
+  Water: 4,
   Leaves: 11,
   GrassPlant: 12,
   Kelp: 13,
