2600dd819dde16a49ac7e0f4a4edbd2ff5fc508c
Kevin Ingersoll <kingersoll@gmail.com>
2022-11-10 16:08:32 -0800

    fix: scale tile hover with map zoom

--- packages/client/src/layers/phaser/systems/createTileHoverSystem.ts
+++ packages/client/src/layers/phaser/systems/createTileHoverSystem.ts
@@ -1,7 +1,6 @@
 import { defineRxSystem } from "@latticexyz/recs";
-import { BehaviorSubject, map } from "rxjs";
 import { NetworkLayer } from "../../network";
-import { TILE_HEIGHT, TILE_WIDTH } from "../constants";
+import { TILE_WIDTH } from "../constants";
 import { PhaserLayer } from "../types";
 
 export function createTileHoverSystem(context: PhaserLayer, _network: NetworkLayer) {
@@ -16,22 +15,20 @@ export function createTileHoverSystem(context: PhaserLayer, _network: NetworkLay
   const sprite = phaserScene.add.sprite(0, 0, "tileHover").setOrigin(0, 0).setVisible(false);
   sprite.depth = 1000;
 
-  const pointerPosition$ = new BehaviorSubject({ x: 0, y: 0 });
-  input.pointermove$.pipe(map(({ pointer }) => ({ x: pointer.worldX, y: pointer.worldY }))).subscribe(pointerPosition$);
-
   defineRxSystem(world, zoomLevel$, ({ zoomMultiplier }) => {
-    sprite.setSize(TILE_WIDTH * zoomMultiplier, TILE_HEIGHT * zoomMultiplier);
-    // TODO: reset position now that zoom level has changed?
+    sprite.setScale(zoomMultiplier);
+    sprite.setPosition(
+      Math.floor(sprite.x / sprite.displayWidth) * sprite.displayWidth,
+      Math.floor(sprite.y / sprite.displayHeight) * sprite.displayHeight
+    );
   });
 
   defineRxSystem(world, input.pointermove$, async ({ pointer }) => {
-    // console.log("got pointer position", pointer);
     sprite.setVisible(true);
     // TODO: hide after at timeout?
-    // TODO: figure out how to remount when map changes due to zoom step
     sprite.setPosition(
-      Math.floor(pointer.worldX / sprite.width) * sprite.width,
-      Math.floor(pointer.worldY / sprite.height) * sprite.height
+      Math.floor(pointer.worldX / sprite.displayWidth) * sprite.displayWidth,
+      Math.floor(pointer.worldY / sprite.displayHeight) * sprite.displayHeight
     );
   });
 }
