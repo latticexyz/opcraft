3d88a0d4bff666ecc228d918c77d84116fbdc2f0
Curtis Chong <curtischong5@gmail.com>
2023-05-26 01:51:46 -0400

    feat: nand test (#19)

---
+++ packages/client/src/layers/react/components/SubmitNandTest.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import styled from "styled-components";
+import { Button, CloseableContainer, Gold } from "./common";
+import {Layers} from "../../../types";
+import {keccak256, VoxelCoord} from "@latticexyz/utils";
+import {defineRxSystem, EntityID, getComponentValue} from "@latticexyz/recs";
+import {toast} from "react-toastify";
+import {distinct} from "rxjs";
+
+export const SubmitNandTest: React.FC<{ onClose: () => void; layers: Layers}> = ({ onClose, layers }) => {
+	const {
+		network: {
+			api,
+			components: {EntityId, PassesTests}, // so far only creations set this entityid field rn
+		},
+		noa: {
+			world,
+			components: { VoxelSelection },
+			SingletonEntity,
+		},
+	} = layers;
+
+	const [creationIds, setCreationIds] = React.useState<string[]>();
+	const [passesTests, setPassesTests] = React.useState<string[]>();
+	React.useEffect(() => {
+		setCreationIds(Array.from(EntityId.values.value.values()));
+		defineRxSystem(world, EntityId.update$.pipe(distinct()), (update) => {
+				setCreationIds(Array.from(EntityId.values.value.values()));
+		});
+
+		const nandTestId =  keccak256("system.NandTest") as EntityID;
+
+		if(PassesTests.values.value.has(nandTestId)) {
+			setPassesTests(Array.from(PassesTests.values.value.get(nandTestId)));
+		}
+		defineRxSystem(world, PassesTests.update$.pipe(distinct()), (update) => {
+			debugger;
+			if(PassesTests.values.value.has(nandTestId)) {
+				setPassesTests(Array.from(PassesTests.values.value.get(nandTestId)));
+			}
+		});
+	}, []);
+
+	const submit = (creationId: string) => {
+		let points: VoxelCoord[] = getComponentValue(VoxelSelection, SingletonEntity)?.points ?? [];
+		// only take the last 3 points to submit the test
+		points = points.slice(-3);
+		if(points.length < 3) {
+			toast("Please select at least 3 points to submit the nand test.")
+			return;
+		}
+
+		api.submitNandTest(creationId, points);
+	}
+
+	return (
+		<ImportContainer onClose={onClose}>
+			<p>
+				<Gold>Submit Nand Test</Gold>
+			</p>
+				<IdContainer>
+			{
+				creationIds?.map((id) => {
+					return (
+							<Button key={`creation-id-${id}`} onClick={() => submit(id)}>Submit 0x{id.slice(0,7)}...</Button>
+					)
+				})
+			}
+				</IdContainer>
+			{
+				passesTests && passesTests.length > 0 && (
+				<div>
+					<p>Creation that passed</p>
+					{
+						passesTests?.map((id) => {
+							return (
+								<div key={`creation-id-${id}`}>0x{id.slice(0,7)}...</div>
+							)
+						})
+					}
+				</div>
+				)
+			}
+
+
+		</ImportContainer>
+	);
+};
+
+const ImportContainer = styled(CloseableContainer)`
+  line-height: 1;
+  pointer-events: all;
+  min-width: 200px;
+`;
+
+const IdContainer = styled.div`
+  margin-top: 8px;
+  display: grid;
+  grid-gap: 9px;
+  grid-auto-flow: column;
+`;
---
+++ packages/contracts/src/systems/NandTestSystem.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0;
+import "solecs/System.sol";
+import { IWorld } from "solecs/interfaces/IWorld.sol";
+import { getAddressById } from "solecs/utils.sol";
+
+import { VoxelsComponent, ID as VoxelsComponentID } from "../components/VoxelsComponent.sol";
+import { PassesTestsComponent, ID as PassesTestsComponentID } from "../components/PassesTestsComponent.sol";
+import { NameComponent, ID as NameComponentID } from "../components/NameComponent.sol";
+import { SignalComponent, ID as SignalComponentID, SignalData } from "../components/SignalComponent.sol";
+import { SignalSourceComponent, ID as SignalSourceComponentID } from "../components/SignalSourceComponent.sol";
+import { PositionComponent, ID as PositionComponentID } from "../components/PositionComponent.sol";
+import { BlockInteraction } from "../libraries/BlockInteraction.sol";
+
+import { VoxelCoord } from "../types.sol";
+
+uint256 constant ID = uint256(keccak256("system.NandTest"));
+
+contract NandTestSystem is System {
+  constructor(IWorld _world, address _components) System(_world, _components) {}
+
+  // returns true if the test is passed
+  function execute(bytes memory arguments) public returns (bytes memory) {
+    (uint256 creationId, VoxelCoord memory in1Voxel, VoxelCoord memory in2Voxel, VoxelCoord memory outVoxel) = abi
+      .decode(arguments, (uint256, VoxelCoord, VoxelCoord, VoxelCoord));
+
+    // TODO: do we require all output to be false first?
+
+    PassesTestsComponent passesTestsComponent = PassesTestsComponent(
+      getAddressById(components, PassesTestsComponentID)
+    );
+    SignalComponent signalComponent = SignalComponent(getAddressById(components, SignalComponentID));
+    SignalSourceComponent signalSourceComponent = SignalSourceComponent(
+      getAddressById(components, SignalSourceComponentID)
+    );
+
+    (uint256 in1VoxelId, uint256 in2VoxelId, uint256 outVoxelId) = getVoxelIds(in1Voxel, in2Voxel, outVoxel);
+
+    signalSourceComponent.set(in1VoxelId, true);
+    signalSourceComponent.set(in2VoxelId, true);
+
+    BlockInteraction.runInteractionSystems(world.systems(), components, in1VoxelId); // TODO: add a way to add both voxels onto the interaction queue
+    BlockInteraction.runInteractionSystems(world.systems(), components, in2VoxelId);
+
+    if (!signalComponent.getValue(outVoxelId).isActive) {
+      passesTestsComponent.addTest(ID, creationId);
+      return abi.encode(true); // if the transaction passes, the test passed!
+    }
+    require(false, "NandTest failed"); // this is really hacky but by failing the transaction, the user gets immediate feedback in the ui
+    // the test failed
+    return abi.encode(false);
+  }
+
+  function getVoxelIds(
+    VoxelCoord memory in1Voxel,
+    VoxelCoord memory in2Voxel,
+    VoxelCoord memory outVoxel
+  )
+    private
+    view
+    returns (
+      uint256,
+      uint256,
+      uint256
+    )
+  {
+    PositionComponent positionComponent = PositionComponent(getAddressById(components, PositionComponentID));
+    uint256[] memory in1VoxelIds = positionComponent.getEntitiesWithValue(in1Voxel);
+    uint256[] memory in2VoxelIds = positionComponent.getEntitiesWithValue(in2Voxel);
+    uint256[] memory outVoxelIds = positionComponent.getEntitiesWithValue(outVoxel);
+
+    require(in1VoxelIds.length == 1, "in1VoxelIds.length != 1");
+    require(in2VoxelIds.length == 1, "in2VoxelIds.length != 1");
+    require(outVoxelIds.length == 1, "outVoxelIds.length != 1");
+
+    return (in1VoxelIds[0], in2VoxelIds[0], outVoxelIds[0]);
+  }
+
+  function executeTyped(
+    uint256 creationId,
+    VoxelCoord memory in1Voxel,
+    VoxelCoord memory in2Voxel,
+    VoxelCoord memory outVoxel
+  ) public returns (bool) {
+    return abi.decode(execute(abi.encode(creationId, in1Voxel, in2Voxel, outVoxel)), (bool));
+  }
+}
